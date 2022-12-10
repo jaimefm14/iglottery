072f7043334d09b06fb0eb90c38f7a6d19706659
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2022-12-10 13:32:20 +0100

    removed acces token

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 
-// const APP_ID = '1133973920641097';
-// console.log('fetch');
-//   fetch(`https://www.facebook.com/v15.0/me/accounts?access_token=EAAQHVZBC5cEkBAA5WgSE0ZBOi0TSEmJnzkxipdcTH5aEK8KMxYXEplUCiFuhlMpb97zxwilbgRsn6sWmJUacv2JLhSLkjOUgTn7y6qF9KSiIJlsQQmAcyFf7vBifln5iVFha0AvaGx6y7Qr6WqGlSVUrUolfCPyxzH1iAXeGBpGu8HFZBhMoFALPBQYtmfr5oJZBRxn2EQZDZD`, {mode: 'no-cors'}).then((response)=>{
-//     console.log('respuesta');
-//     console.log(response);
-//   })
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,29 +12,6 @@ function App() {
 
 
 
-
-
-  // console.log('fetch');
-  // fetch(`https://www.facebook.com/v15.0/dialog/oauth?client_id=${APP_ID}&redirect_uri=https://www.facebook.com/connect/login_success.html`).then((response)=>{
-  //   console.log('respuesta');
-  //   console.log(response);
-  // })
-
-
-  // console.log('fetch');
-  // fetch(`https://www.facebook.com/v15.0/me/accounts?access_token=EAAQHVZBC5cEkBAMQ76ZA21CSnfQbB2EvXAuIhkRUQWC1naRovsC0gNglCiwEjwGPYBl8wlSMRhCXaYgDUrzvYeEsiaFTwoPlvrX2hsOQeCB15UTtZBmVpY2uPPnvtbqiu4PX2DCZB4iyYIZCZAWal5uIsZC64S6OdXutwJZCVMfvXIXRufkSzWGG83ecaRsoYIxEdD4nMUqPX6Rwa5ZAna0DiTRsGdIAzXdRb7RkNra3DjE2miGyxDh8e`, {mode: 'no-cors'}).then((response)=>{
-  //   console.log('respuesta');
-  //   console.log(response);
-  // })
-
-
-
-  // me/accounts?access_token=EAAQHVZBC5cEkBAMQ76ZA21CSnfQbB2EvXAuIhkRUQWC1naRovsC0gNglCiwEjwGPYBl8wlSMRhCXaYgDUrzvYeEsiaFTwoPlvrX2hsOQeCB15UTtZBmVpY2uPPnvtbqiu4PX2DCZB4iyYIZCZAWal5uIsZC64S6OdXutwJZCVMfvXIXRufkSzWGG83ecaRsoYIxEdD4nMUqPX6Rwa5ZAna0DiTRsGdIAzXdRb7RkNra3DjE2miGyxDh8e
-
-
-
-
-  // https://www.facebook.com/v15.0/dialog/oauth?client_id=1133973920641097&redirect_uri="https://www.facebook.com/connect/login_success.html"&state={"{st=state123abc,ds=123456789}"}
 
 
 
